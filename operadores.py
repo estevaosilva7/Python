3c72bd1441f3a07e0ce22c0b0d3f0ddf083df151
Estevão <113836244+estevaosilva7@users.noreply.github.com>
2025-07-21 08:58:59 -0300

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,36 @@
+a = 10
+b = 2
+
+
+# Aritméticos
+
+print(a + b) # soma
+print(a - b) # subtração
+print(a * b) # multiplicação
+print(a / b) # divisão (resultado com float)
+print(a // b) # divisão inteira
+print(a % b) # resto da divisão 
+print(a ** b) # potência
+print("==================")
+
+
+# Relacionais
+
+print(a > b) # maior que 
+print(a < b) # menor que
+print(a == b) # igual
+print(a != b) # diferente
+print(a >= b) # maior ou igual
+print(a <= b) # menor ou igual
+print("==================")
+
+
+# Lógicos
+
+x = True
+y = False
+
+
+print(x and y) # E (True se ambos forem True)
+print(x or y) # OU (True se pelo menos um for True)
+print(not x) # NÃO (Inverte o valor)
---
+++ variaveis.py
@@ -0,0 +1,9 @@
+nome = "Estevão" #string
+idade = 99 #int
+altura = 2.50 #float
+status = True #bool
+
+print("Nome: ", nome)
+print("Idade: ", idade)
+print("Altura: ", altura)
+print("Status: ", status)
